22f792b18e77f1d787418b9813ba1473492580dc
DerekMMartin <Derekmikelmartin@gmail.com>
2018-08-22 14:43:59 -0600

    CameraEmbeddedFragment Completed. - Needs Something done with pictures taken Log out button User Email displayed on the drawer.

---
+++ app/src/main/java/com/example/derekmartin/whereyouatreloaded/CameraEmbeddedFragment.java
@@ -0,0 +1,280 @@
+package com.example.derekmartin.whereyouatreloaded;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.util.Size;
+import android.view.LayoutInflater;
+import android.view.Surface;
+import android.view.TextureView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.Executor;
+
+
+public class CameraEmbeddedFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    private static final int CAMERA_REQUEST_CODE = 200;
+    private View MyView;
+    private CameraManager cameraManager;
+    private int cameraFacing;
+    private TextureView.SurfaceTextureListener surfaceTextureListener;
+    private Size previewSize;
+    private String cameraId;
+    private CameraDevice.StateCallback stateCallback;
+    private Handler backgroundHandler;
+    private HandlerThread backgroundThread;
+    private CameraDevice cameraDevice;
+    private CaptureRequest.Builder captureRequestBuilder;
+    private CaptureRequest captureRequest;
+    private CameraCaptureSession cameraCaptureSession;
+    private File galleryFolder;
+
+    public CameraEmbeddedFragment() {
+        // Required empty public constructor
+    }
+    public void onTakePhotoButtonClicked() {
+        lock();
+        FileOutputStream outputPhoto = null;
+        try {
+            outputPhoto = new FileOutputStream(createImageFile(galleryFolder));
+            ((TextureView)(MyView.findViewById(R.id.texture_view))).getBitmap()
+                    .compress(Bitmap.CompressFormat.PNG, 100, outputPhoto);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            unlock();
+            try {
+                if (outputPhoto != null) {
+                    outputPhoto.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    private void createImageGallery() {
+        File storageDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        galleryFolder = new File(storageDirectory, "Where You At Reloaded");
+        if (!galleryFolder.exists()) {
+            boolean wasCreated = galleryFolder.mkdirs();
+            if (!wasCreated) {
+                Log.e("CapturedImages", "Failed to create directory");
+            }
+        }
+    }
+    private File createImageFile(File galleryFolder) throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        String imageFileName = "image_" + timeStamp + "_";
+        return File.createTempFile(imageFileName, ".jpg", galleryFolder);
+    }
+    private void createPreviewSession() {
+        try {
+            SurfaceTexture surfaceTexture = ((TextureView)(MyView.findViewById(R.id.texture_view))).getSurfaceTexture();
+            surfaceTexture.setDefaultBufferSize(previewSize.getWidth(), previewSize.getHeight());
+            Surface previewSurface = new Surface(surfaceTexture);
+            captureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            captureRequestBuilder.addTarget(previewSurface);
+
+            cameraDevice.createCaptureSession(Collections.singletonList(previewSurface),
+                    new CameraCaptureSession.StateCallback() {
+
+                        @Override
+                        public void onConfigured(CameraCaptureSession cameraCaptureSession) {
+                            if (cameraDevice == null) {
+                                return;
+                            }
+
+                            try {
+                                captureRequest = captureRequestBuilder.build();
+                                CameraEmbeddedFragment.this.cameraCaptureSession = cameraCaptureSession;
+                                CameraEmbeddedFragment.this.cameraCaptureSession.setRepeatingRequest(captureRequest,
+                                        null, backgroundHandler);
+                            } catch (CameraAccessException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        @Override
+                        public void onConfigureFailed(CameraCaptureSession cameraCaptureSession) {
+
+                        }
+                    }, backgroundHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+    private void openBackgroundThread() {
+        backgroundThread = new HandlerThread("camera_background_thread");
+        backgroundThread.start();
+        backgroundHandler = new Handler(backgroundThread.getLooper());
+    }
+    private void lock() {
+        try {
+            cameraCaptureSession.capture(captureRequestBuilder.build(),
+                    null, backgroundHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+    private void unlock() {
+        try {
+            cameraCaptureSession.setRepeatingRequest(captureRequestBuilder.build(),
+                    null, backgroundHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+    private void setUpCamera() {
+        try {
+            for (String cameraId : cameraManager.getCameraIdList()) {
+                CameraCharacteristics cameraCharacteristics =
+                        cameraManager.getCameraCharacteristics(cameraId);
+                if (cameraCharacteristics.get(CameraCharacteristics.LENS_FACING) ==
+                        cameraFacing) {
+                    StreamConfigurationMap streamConfigurationMap = cameraCharacteristics.get(
+                            CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+                    previewSize = streamConfigurationMap.getOutputSizes(SurfaceTexture.class)[0];
+                    this.cameraId = cameraId;
+                }
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+    private void openCamera() {
+        try {
+            if (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.CAMERA)
+                    == PackageManager.PERMISSION_GRANTED) {
+                cameraManager.openCamera(cameraId, stateCallback, backgroundHandler);
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        MyView = inflater.inflate(R.layout.fragment_camera_embedded, container, false);
+        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, CAMERA_REQUEST_CODE);
+        createImageGallery();
+
+        cameraManager = (CameraManager) getActivity().getSystemService(Context.CAMERA_SERVICE);
+        cameraFacing = CameraCharacteristics.LENS_FACING_BACK;
+
+        surfaceTextureListener = new TextureView.SurfaceTextureListener() {
+            @Override
+            public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int width, int height) {
+                setUpCamera();
+                openCamera();
+            }
+
+            @Override
+            public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int width, int height) {
+
+            }
+
+            @Override
+            public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
+                return false;
+            }
+
+            @Override
+            public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
+
+            }
+        };
+        stateCallback = new CameraDevice.StateCallback() {
+            @Override
+            public void onOpened(CameraDevice cameraDevice) {
+                CameraEmbeddedFragment.this.cameraDevice = cameraDevice;
+                createPreviewSession();
+            }
+
+            @Override
+            public void onDisconnected(CameraDevice cameraDevice) {
+                cameraDevice.close();
+                CameraEmbeddedFragment.this.cameraDevice = null;
+            }
+
+            @Override
+            public void onError(CameraDevice cameraDevice, int error) {
+                cameraDevice.close();
+                CameraEmbeddedFragment.this.cameraDevice = null;
+            }
+        };
+        MyView.findViewById(R.id.capture_photo_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onTakePhotoButtonClicked();
+            }
+        });
+        return  MyView;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        openBackgroundThread();
+        if (((TextureView)(MyView.findViewById(R.id.texture_view))).isAvailable()) {
+            setUpCamera();
+            openCamera();
+        } else {
+            ((TextureView)(MyView.findViewById(R.id.texture_view))).setSurfaceTextureListener(surfaceTextureListener);
+        }
+    }
+    @Override
+    public void onStop(){
+        backgroundThread.quitSafely();
+        super.onStop();
+    }
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
--- app/src/main/java/com/example/derekmartin/whereyouatreloaded/MainActivity.java
+++ app/src/main/java/com/example/derekmartin/whereyouatreloaded/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.derekmartin.whereyouatreloaded;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -9,21 +10,34 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends FragmentActivity
         implements
         FirstFragment.OnFragmentInteractionListener,
         SecondFragment.OnFragmentInteractionListener,
         FriendsFragment.OnFragmentInteractionListener,
-        CameraFragment.OnFragmentInteractionListener
+        CameraFragment.OnFragmentInteractionListener,
+        CameraEmbeddedFragment.OnFragmentInteractionListener
 {
 
     private DrawerLayout dLayout;
     private android.support.v4.app.FragmentManager fManager;
+
+    private void LogOutAndSwitch() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,13 +46,16 @@ public class MainActivity extends FragmentActivity
 
         fManager=getSupportFragmentManager();
 
-        //initializes to the first fragment in menu and selects it
-//        NavigationView nv= findViewById(R.id.nav_view);
-//        nv.setCheckedItem(R.id.menufirst);
-//        changeFragment(new FirstFragment());
-        //TODO Why is there a duplicate
         dLayout=findViewById(R.id.drawerLayout);
         NavigationView nView = findViewById(R.id.nav_view);
+
+        ((TextView)nView.findViewById(R.id.EmailDisplay)).setText(FirebaseAuth.getInstance().getCurrentUser().getEmail());
+        ((Button)nView.findViewById(R.id.LogOutButton)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LogOutAndSwitch();
+            }
+        });
         nView.setCheckedItem(R.id.menufirst);
         changeFragment(new FirstFragment());
 
@@ -62,8 +79,8 @@ public class MainActivity extends FragmentActivity
                             case R.id.menuCamera:
                                 changeFragment(new CameraFragment());
                                 break;
-                            case R.id.menuCameraIntegrated:
-                                changeFragment(new CameraFragmentIntegration());
+                            case R.id.menuCameraEmbedded:
+                                changeFragment(new CameraEmbeddedFragment());
                                 break;
                         }
 
